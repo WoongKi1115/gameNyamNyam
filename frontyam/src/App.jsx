79ad5a1ff399ff70637bbd7ebcdc32876686f75f
WoongKi1115 <nwk6638@gmail.com>
2023-03-13 17:57:21 +0900

    Update : mainpage
    Former-commit-id: ed23cf8d34e7e440dc9c5226f7303f71fe466e36

---
+++ frontyam/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {mainpage} from "../public/진짜 로그인 전.png";
+
+export default function App() {
+    return (
+        <div>
+            <img src={mainpage} alt="" />
+        </div>
+    );
+}
