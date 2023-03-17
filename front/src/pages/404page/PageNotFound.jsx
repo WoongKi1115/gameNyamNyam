7e09e019483572cd4b17b009c31a081b1b894a13
kimchangyoung <kyle.kcy@gmail.com>
2023-03-17 11:27:12 +0900

    feat/route
    Former-commit-id: 6b8039c564765544a4660bd86b004b8df57be47f

--- front/src/pages/404page/PageNotFound.jsx
+++ front/src/pages/404page/PageNotFound.jsx
@@ -7,7 +7,9 @@ export default function NotFound() {
         src="./src/assets/404.png"
         className="absolute object-cover w-full h-full"
       />
+      {/* 배경색 조정 */}
       <div className="absolute inset-0 bg-black opacity-25"></div>
+      {/* 배경 글자 */}
       <div className="container relative z-10 flex items-center px-6 py-32 mx-auto md:px-12 xl:py-40">
         <div className="relative z-10 flex flex-col items-center w-full font-mono">
           <h1 className="mt-4 text-5xl font-extrabold leading-tight text-center text-white">
