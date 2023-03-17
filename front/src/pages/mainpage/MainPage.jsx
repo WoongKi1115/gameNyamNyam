4abb96a9764421ab642a69950e15478ccf24316f
nwk2222 <nwk2222@naver.com>
2023-03-17 12:02:59 +0900

    feat: door open
    Former-commit-id: 011ff5ca07d2cd98869f578bf54bbeabddeb8885

--- front/src/pages/mainpage/MainPage.jsx
+++ front/src/pages/mainpage/MainPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
-
+import React, { useRef } from 'react';
+import gsap from 'gsap';
 export default function Mainpage() {
+  const leftDoorRef = useRef(null);
+  const rightDoorRef = useRef(null);
+
+  function doorOpen() {
+    gsap
+      .timeline()
+      .to(leftDoorRef.current, { duration: 1, x: -200 })
+      .to(rightDoorRef.current, { duration: 1, x: 200 }, '-=1');
+  }
+  function doorClose() {
+    gsap
+      .timeline()
+      .to(leftDoorRef.current, { duration: 1, x: 0 })
+      .to(rightDoorRef.current, { duration: 1, x: 0 }, '-=1');
+  }
+
   return (
     <div className="mainPageImg">
-      <div className="steamLoginBtn">
+      <div className="door" onClick={doorClose}>
+        <div className="leftDoor" ref={leftDoorRef}></div>
+        <div className="rightDoor" ref={rightDoorRef}></div>
+      </div>
+      <div className="steamLoginBtn" onClick={doorOpen}>
         <div className="steamTxt">continue with steam login</div>
         <div className="steamImg"></div>
       </div>
