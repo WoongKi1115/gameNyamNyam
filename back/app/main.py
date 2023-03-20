4d273c6e638fbd7c5dd81b759e6238d23c28861a
LeeSuA <huimhuimi@naver.com>
2023-03-20 13:53:43 +0900

    feat: 스팀 로그인 & 아이디 반환
    Former-commit-id: 997d921287183b0bcd0355161e5e48c7b5b9952a

--- back/app/main.py
+++ back/app/main.py
@@ -1,14 +1,17 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Depends
+from starlette.requests import Request
+from steamsignin import SteamSignIn
 
 app = FastAPI()
+api_url = "http://127.0.0.1:8000"
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get('/login')
+async def main(steam_signin: SteamSignIn = Depends(SteamSignIn)):
+    url = steam_signin.ConstructURL(api_url+'/processlogin')
+    return steam_signin.RedirectUser(url)
 
 
-@app.get("/users/")
-def read_userinfo():
-    return {"Hello": "World"}
+@app.get('/processlogin')
+async def pr(request: Request, steam_signin: SteamSignIn = Depends(SteamSignIn)):
+	return steam_signin.ValidateResults(request.query_params)
