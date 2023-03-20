4597477293d410cd23abcd48577fb11dc3387cbd
nwk2222 <nwk2222@naver.com>
2023-03-20 16:41:11 +0900

    feat:plate
    Former-commit-id: e24b0183e1a8ff11320a04cc54c3443e0e446c2f

--- front/src/pages/mainpage/MainPage.jsx
+++ front/src/pages/mainpage/MainPage.jsx
@@ -1,26 +1,38 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import gsap from 'gsap';
+import { useNavigate } from 'react-router-dom';
+
 export default function Mainpage() {
   const leftDoorRef = useRef(null);
   const rightDoorRef = useRef(null);
+  const [isLogin, setLogin] = useState(false);
+  const navigate = useNavigate();
+
+  const navigateToGame = () => {
+    if (isLogin) {
+      navigate('/game');
+    }
+  };
 
   function doorOpen() {
     gsap
       .timeline()
       .to(leftDoorRef.current, { duration: 1, x: -250 })
       .to(rightDoorRef.current, { duration: 1, x: 250 }, '-=1');
+    setLogin(true);
   }
-  function doorClose() {
-    gsap
-      .timeline()
-      .to(leftDoorRef.current, { duration: 1, x: 0 })
-      .to(rightDoorRef.current, { duration: 1, x: 0 }, '-=1');
-  }
+  // function doorClose() {
+  //   gsap
+  //     .timeline()
+  //     .to(leftDoorRef.current, { duration: 1, x: 0 })
+  //     .to(rightDoorRef.current, { duration: 1, x: 0 }, '-=1');
+  // }
 
   return (
     <div className="mainPageImg">
-      <div className='kanban'></div>
-      <div className="door" onClick={doorClose}>
+      <div className="kanban"></div>
+
+      <div className="door" onClick={navigateToGame}>
         <div className="leftDoor" ref={leftDoorRef}>
           <div className="insideDoor"></div>
         </div>
@@ -28,6 +40,7 @@ export default function Mainpage() {
           <div className="insideDoor"></div>
         </div>
       </div>
+
       <div className="steamLoginBtn" onClick={doorOpen}>
         <div className="steamTxt">continue with steam login</div>
         <div className="steamImg"></div>
