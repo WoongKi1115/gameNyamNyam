60ed6d3c6521e2164b9e24e30b4fc83d90ab33cc
leedoyeon <ldy__0202@naver.com>
2023-03-22 10:47:02 +0900

    feat: 성향 분석
    Former-commit-id: 54a7dae28ca64709037ac05f16168cceb97302e2

--- back/app/api/endpoints/recommendations.py
+++ back/app/api/endpoints/recommendations.py
@@ -2,6 +2,31 @@ import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
 '''
+Input: 예전에 구매한 게임
+Output: 분석 취향
+'''
+def get_preference(games):
+    
+    # 구매 기록 게임으로부터 장르 전체 가져오기
+    genre_list = []
+    for game in games:
+        genre_list.extend(game['genres'].split(', '))
+
+    # 장르 카운트하기
+    genre_count_dict = {}
+    for genre in set(genre_list):
+        count = genre_list.count(genre)
+        genre_count_dict[f'{genre}'] = count
+    
+    # 개수대로 정렬하기
+    sorted_dict = dict(sorted(genre_count_dict.items(), key=lambda x: x[1], reverse=True))
+    
+    # 상위 5개 뽑기
+    result = list(sorted_dict.keys())[:5]
+
+    return result
+
+'''
 Input: 분석된 취향, 게임들
 Output: 게임별 매치율 리스트
 
@@ -37,10 +62,16 @@ Output: 비슷한 게임 appid 리스트.
 TO DO : 
 비슷한 게임 구하기.
 
+1번 - 카테고리를 사용한 알고리즘
 (1) 해당 게임들(장바구니)로부터 성향 분석
-(2) 도출된 성향과 일치하는 게임 추천(전체에서?)
+(2) 도출된 성향과 일치하는 게임 추천(전체에서)
+
+2번 - desc를 사용한 알고리즘
+(1) 해당 게임들의 about_game 분석
+(2) 일치하는 게임 추천 (전체에서)
+
 '''
-def get_similar():
+def get_similar(games):
     result = []
     return result
 
