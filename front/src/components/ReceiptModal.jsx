aa12786abbd616eb84e0e2f86c48a2dbf776deba
kimchangyoung <kyle.kcy@gmail.com>
2023-03-22 17:57:18 +0900

    feat:modal plus
    Former-commit-id: 8a8c91cf5d5623f2e5796cb2fb0095365d866999

---
+++ front/src/components/ReceiptModal.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Modal({ setreceipt }) {
+  return (
+    <div className='h-screen w-full fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-30 text-center z-10'
+    onClick={setreceipt}>
+        <img src="./src/assets/receipt.png" alt="" />
+    </div>
+  );
+}
