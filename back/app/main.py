55bd5cd203751ea65f8efc90639214f44b342e5b
LeeSuA <huimhuimi@naver.com>
2023-03-22 09:56:20 +0900

    feat: game detail 조회 [#218]
    Former-commit-id: 86d9de70467c3b02076f449f7692fd382143f0a8

--- back/app/main.py
+++ back/app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
-
+import requests
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/games/{game_id}")
+async def get_game_info(game_id: int):
+    url = f"https://store.steampowered.com/api/appdetails?appids={game_id}"
+    response = requests.get(url)
+    json_response = response.json()
+    game_info = json_response[str(game_id)]["data"]
+    return game_info
