f3365f7abbe09fec0773dc0589c52fa6d5c3a184
LeeSuA <huimhuimi@naver.com>
2023-03-22 09:49:37 +0900

    feat: 총 플레이 게임 수 리턴 [#217]
    Former-commit-id: 88173dec585ec444efae066627bf42f8b5bc8b6b

--- back/app/main.py
+++ back/app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
-
+import requests
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/games/{user_id}")
+async def get_game_count(user_id: str):
+    url = f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=FF1F5EF115E50CEBE0A7C11123959310&steamid={user_id}&format=json"
+    response = requests.get(url)
+    json_response = response.json()
+    game_count = json_response["response"]["game_count"]
+    return game_count
