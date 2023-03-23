71ecdcedfbda97771ce2dd72ac76cda906acd07b
kimchangyoung <kyle.kcy@gmail.com>
2023-03-23 17:21:29 +0900

    feat.modal
    Former-commit-id: 429a39c87fcec8528447734889d4fcd3b933ff35

--- front/src/components/AddGameModal.jsx
+++ front/src/components/AddGameModal.jsx
@@ -3,7 +3,7 @@ import ImgBox from './Imgbox';
 
 export default function gamemodal() {
     return(
-        <div className='border-2 absolute inset-x-0 bottom-0 h-48 flex justify-center space-x-3 bg-black'>
+        <div className='border-2 absolute inset-x-0 bottom-0 h-2/6 flex justify-center space-x-3 bg-black bg-opacity-50'>
             <ImgBox />
             <ImgBox />
             <ImgBox />
