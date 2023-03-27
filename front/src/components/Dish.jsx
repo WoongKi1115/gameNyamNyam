77d3875546d78999d1afb5648cd2c184f67c49df
nwk2222 <nwk2222@naver.com>
2023-03-27 15:10:08 +0900

    feat: game page
    Former-commit-id: 266a11f778d8f009050fdcb53811e2b1eef85c97

--- front/src/components/Dish.jsx
+++ front/src/components/Dish.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
-export default function Dish() {
+export default function Dish({ price }) {
+  const changeColor = (price) => {
+    console.log(price);
+    if (price === 0) {
+      return '#FEF874';
+    } else if (0 < price && price <= 10000) {
+      return '#92C63A';
+    } else if (10000 < price && price <= 30000) {
+      return '#DB4646';
+    } else if (30000 < price && price <= 50000) {
+      return '#454FAA';
+    } else if (50000 < price) {
+      return '#373737';
+    }
+  };
+
+  const backgroundColor = changeColor(price);
+
   return (
     <div className="gamePlate">
-      <div className="plate1">
-        <div className="plate2">
+      <div className="plate1" style={{ backgroundColor: backgroundColor }}>
+        <div className="plate2" style={{ backgroundColor: backgroundColor }}>
           <div className="plate3">
             <div className="gamePic"></div>
           </div>
