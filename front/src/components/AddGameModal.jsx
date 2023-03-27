b437980b032780c481acb78f71c05700d64a256b
kimchangyoung <kyle.kcy@gmail.com>
2023-03-27 14:25:41 +0900

    test
    Former-commit-id: f9c3da3efe24bd0a6cdda7debfd8098a889e0293

--- front/src/components/AddGameModal.jsx
+++ front/src/components/AddGameModal.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ImgBox from './Imgbox';
 
 export default function gamemodal() {
-    return(
-        <div className='border-2 absolute inset-x-0 bottom-0 h-2/6 flex justify-center space-x-3 bg-black bg-opacity-50'>
-            <ImgBox />
-            <ImgBox />
-            <ImgBox />
-            <ImgBox />
-        </div>
-    );
+  return (
+    <div className="absolute inset-x-0 bottom-0 flex justify-center space-x-3 bg-black bg-opacity-80 gap-4 py-4">
+      <a href="https://www.naver.com" target="_blank" rel="noopener noreferrer" className='mx-4'>
+        <ImgBox />
+      </a>
+      <a href="https://www.naver.com" target="_blank" rel="noopener noreferrer" className='mx-4'>
+        <ImgBox />
+      </a>
+      <a href="https://www.naver.com" target="_blank" rel="noopener noreferrer" className='mx-4'>
+        <ImgBox />
+      </a>
+      <a href="https://www.naver.com" target="_blank" rel="noopener noreferrer" className='mx-4'>
+        <ImgBox />
+      </a>
+    </div>
+  );
 }
