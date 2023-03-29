5a7665c04a426d391fa5ad8fd1b03c30f52ea5d4
이수아 <huimhuimi@naver.com>
2023-03-29 11:15:20 +0900

    feat: 상위 genre 5개 조회
    Former-commit-id: bd600e27b5a5402bda016f42a3388b1b4dad2ab3

--- back/app/main.py
+++ back/app/main.py
@@ -13,6 +13,7 @@ import os
 
 load_dotenv()
 mongodb_root = os.environ.get('mongodb_root')
+steam_key = os.getenv('steam_key')
 mongodb_URI = f"mongodb+srv://root:{mongodb_root}@gamenyamnaym.t2iixnv.mongodb.net/test"
 
 # DB 연결
@@ -26,8 +27,6 @@ games = db['game']
 
 
 app = FastAPI()
-# db 연동
-connect(db="nyamnyam", host="localhost", port=27017)
 api_url = "http://127.0.0.1:8000"
 
 
@@ -60,8 +59,40 @@ def get_all_game():
 
 
 @app.get("/games/result/{userid}")
-def get_game_by_tag():
-    return {"Hello": "World"}
+def get_game_by_tag(userid: str):
+    """ 5개 이상 플레이한 사용자가 전에 했던 게임 기반으로 태그 분석
+
+    Args:
+        userid : 유저 아이디
+
+    Returns:
+        전에 플레이했던 장르 중 가장 많은 태그 5개
+
+    """
+
+    # 1. 플레이했던 게임 정보(appid) 불러오기
+    playedGames = {}
+    appids = []
+    playedGenres = []
+    result = {}
+    url = f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={steam_key}&steamid={userid}"
+    response = requests.get(url)
+    json_response = response.json()
+    playedGames = json_response["response"]["games"]
+
+    for k in playedGames:
+        appids.append(k['appid'])
+
+    # 2. 불러온 appid를 이용해 db에서 game detail 가져오기
+
+    for k in appids:
+        playedGenres.append(games.find_one({"appid": str(k)}, {
+            "_id": 0, "genres": 1}))
+
+    # 3. game detail list에서 장르 뽑아서 5개 가져오기
+    result = recommend_game.get_preference(playedGenres)
+
+    return result
 
 # 5개 미만 플레이한 사용자의 장바구니 기반으로 (태그) 분석
 
@@ -99,9 +130,10 @@ def get_game_detail(appid: str):
         제목, 가격, 스크린샷, 짧은 설명, 개발사, 출시일, 장르, 카테고리
     """
     result = {}
-    
+
     # DB 조회
-    get_game = games.find_one({"appid":appid},{"_id":0, "recommendations":0, "metacritic":0, "about_the_game":0})
+    get_game = games.find_one({"appid": appid}, {
+                              "_id": 0, "recommendations": 0, "metacritic": 0, "about_the_game": 0})
 
     # categories, genres, screenshots, developers 리스트화
     get_game["categories"] = get_game["categories"].split(",")
