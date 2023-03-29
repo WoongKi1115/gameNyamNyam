f5e22f2f4c8ed863ae3c1d9c2c55bdab6cb48a9e
leedoyeon <ldy__0202@naver.com>
2023-03-29 12:09:05 +0900

    fix: key 교체 & 변수명 통일
    Former-commit-id: 905aab3b05a600cfe69e38da8142d8b512e5430c

--- back/app/main.py
+++ back/app/main.py
@@ -44,9 +44,9 @@ app = FastAPI()
 api_url = "http://127.0.0.1:8000"
 
 
-@app.get("/games/{user_id}")
-async def get_game_count(user_id: str):
-    url = f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=FF1F5EF115E50CEBE0A7C11123959310&steamid={user_id}&format=json"
+@app.get("/games/{steamId}")
+async def get_game_count(steamId: str):
+    url = f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={steam_key}&steamid={steamId}&format=json"
     response = requests.get(url)
     json_response = response.json()
     game_count = json_response["response"]["game_count"]
