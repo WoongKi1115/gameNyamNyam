50b80ae0f16a5a89680c58baa514799dfdaeda3d
nwk2222 <nwk2222@naver.com>
2023-03-29 09:49:57 +0900

    feat: changed data
    Former-commit-id: c368bdaba21f22f03aeb66c6f820a2fd85a422c5

--- front/src/components/Dish.jsx
+++ front/src/components/Dish.jsx
@@ -23,7 +23,24 @@ export default function Dish({ price }) {
       <div className="plate1" style={{ backgroundColor: backgroundColor }}>
         <div className="plate2" style={{ backgroundColor: backgroundColor }}>
           <div className="plate3">
-            <div className="gamePic"></div>
+            <div className="gamePic">
+              <div className="gamePicHover">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={2}
+                  stroke="currentColor"
+                  className="searchIcon w-12 h-12 stroke-2 text-white text-xl"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                  />
+                </svg>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- front/src/pages/gamepage/GamePage2.jsx
+++ front/src/pages/gamepage/GamePage2.jsx
@@ -1,36 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Dish from '../../components/Dish';
 import { Droppable, DragDropContext, Draggable } from 'react-beautiful-dnd';
 import Receipt from '../../components/Receipt';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { userGame } from '../../../recoil/user/atoms';
-import Dummy from '../../../DB/front_game_data21.json';
-import axios from 'axios';
+
 
 export default function Gamepage() {
-  useEffect(() => {
-    axios
-      .post('http://127.0.0.1:8000/games/test')
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, []);
-  const testDummy1 = Dummy.data.slice(0, 30);
-  const testDummy2 = Dummy.data.slice(30);
 
-  const FirstSushis = JSON.parse(JSON.stringify(testDummy1));
-  const SecondSushis = JSON.parse(JSON.stringify(testDummy2));
+  const [theEnd, setTheEnd] = useState('false');
   const navigate = useNavigate();
+  const FirstSushis = [
+    { id: '1', name: 'Tuna Sushi', price: 10000 },
+    { id: '2', name: 'Salmon Sushi', price: 30000 },
+    { id: '3', name: 'Eel Sushi', price: 50000 },
+    { id: '4', name: 'Shrimp Sushi', price: 10000 },
+    { id: '5', name: 'Octopus Sushi', price: 10000 },
+    { id: '6', name: 'Uni Sushi', price: 10000 },
+    { id: '7', name: 'Stake Sushi', price: 30000 },
+    { id: '8', name: 'Tamago Sushi', price: 10000 },
+    { id: '9', name: 'Tofu Sushi', price: 10000 },
+    { id: '10', name: 'Flatfish Sushi', price: 50000 },
+  ];
+  const SecondSushis = [
+    { id: '11', name: 'Tuna Sushi', price: 10000 },
+    { id: '12', name: 'Salmon Sushi', price: 10000 },
+    { id: '13', name: 'Eel Sushi', price: 50000 },
+    { id: '14', name: 'Shrimp Sushi', price: 0 },
+    { id: '15', name: 'Octopus Sushi', price: 10000 },
+    { id: '16', name: 'Uni Sushi', price: 50000 },
+    { id: '17', name: 'Stake Sushi', price: 30000 },
+    { id: '18', name: 'Tamago Sushi', price: 0 },
+    { id: '19', name: 'Tofu Sushi', price: 100000 },
+    { id: '20', name: 'Flatfish Sushi', price: 10000 },
+  ];
   const [plates, setPlates] = useRecoilState(userGame);
 
   const onDragStart = (result) => {
     const { source } = result;
-    console.log('testDummy1', FirstSushis);
-    console.log('testDummy2', SecondSushis);
+    console.log(FirstSushis);
+    console.log(SecondSushis);
     console.log('source.droppableId: ', source.droppableId);
     console.log(FirstSushis[source.index]);
     const banneridnum = source.index + 1;
@@ -90,35 +100,34 @@ export default function Gamepage() {
     ) {
       if (source.droppableId === 'first-sushi-bar') {
         const sushi = FirstSushis.find(
-          (sushi) => String(sushi.appid) === result.draggableId,
+          (sushi) => sushi.id === result.draggableId,
         );
-        console.log('sushi.appid', sushi);
         FirstSushis.map((sushi) => {
-          if (String(sushi.appid) === result.draggableId) {
+          if (sushi.id === result.draggableId) {
             const banneridnum = FirstSushis.indexOf(sushi) + 1;
             const getSushi = document.querySelector(
               `#firstPlate${banneridnum}`,
             );
+            console.log(getSushi);
             getSushi.setAttribute('style', 'pointer-events:none');
             getSushi.setAttribute('style', 'visibility:hidden');
-            console.log(getSushi);
           }
         });
         const newPlates = [...plates, sushi];
         setPlates(newPlates);
       } else if (source.droppableId === 'second-sushi-bar') {
         const sushi = SecondSushis.find(
-          (sushi) => String(sushi.appid) === result.draggableId,
+          (sushi) => sushi.id === result.draggableId,
         );
         SecondSushis.map((sushi) => {
-          if (String(sushi.appid) === result.draggableId) {
+          if (sushi.id === result.draggableId) {
             const banneridnum = SecondSushis.indexOf(sushi) + 1;
             const getSushi = document.querySelector(
               `#secondPlate${banneridnum}`,
             );
+            console.log(getSushi);
             getSushi.setAttribute('style', 'pointer-events:none');
             getSushi.setAttribute('style', 'visibility:hidden');
-            console.log(getSushi);
           }
         });
         const newPlates = [...plates, sushi];
@@ -141,7 +150,6 @@ export default function Gamepage() {
       bannerLeft += 100 + 200;
       plate.setAttribute('id', `firstPlate${++imgCnt}`);
     });
-    console.log($first, $last);
 
     if (imgCnt > 9) {
       last = imgCnt;
@@ -149,12 +157,8 @@ export default function Gamepage() {
         $plates.forEach((plate) => {
           plate.style.left = `${plate.offsetLeft - 1}px`;
         });
-        // console.log('first', first);
-        // console.log('last', last);
         $first = document.querySelector(`#firstPlate${first}`);
         $last = document.querySelector(`#firstPlate${last}`);
-        // console.log('$first', $first);
-        // console.log('$last', $last);
         if ($first.offsetLeft < -200) {
           $first.style.left = `${$last.offsetLeft + 100 + 200}px`;
           first++;
@@ -187,8 +191,9 @@ export default function Gamepage() {
       bannerLeft += 100 + 200;
       plate.setAttribute('id', `secondPlate${++imgCnt}`);
     });
-    console.log('imgCnt', imgCnt);
+
     if (imgCnt > 9) {
+      console.log(theEnd);
       last = imgCnt;
       const intervalId = setInterval(() => {
         $plates.forEach((plate) => {
@@ -200,7 +205,6 @@ export default function Gamepage() {
           $first.style.left = `${$last.offsetLeft - 100 - 200}px`;
           first++;
           last++;
-
           if (last > imgCnt) {
             last = 1;
           }
@@ -216,6 +220,7 @@ export default function Gamepage() {
     }
   }, []);
   const navigateToResult = () => {
+    setTheEnd('true');
     navigate('/result');
   };
   return (
@@ -237,8 +242,8 @@ export default function Gamepage() {
                 <div ref={provided.innerRef} {...provided.droppableProps}>
                   {FirstSushis.map((sushi, index) => (
                     <Draggable
-                      key={String(sushi.appid)}
-                      draggableId={String(sushi.appid)}
+                      key={sushi.id}
+                      draggableId={sushi.id}
                       index={index}
                     >
                       {(provided) => (
@@ -270,8 +275,8 @@ export default function Gamepage() {
                 <div ref={provided.innerRef} {...provided.droppableProps}>
                   {SecondSushis.map((sushi, index) => (
                     <Draggable
-                      key={String(sushi.appid)}
-                      draggableId={String(sushi.appid)}
+                      key={sushi.id}
+                      draggableId={sushi.id}
                       index={index}
                     >
                       {(provided) => (
@@ -305,8 +310,8 @@ export default function Gamepage() {
                 {plates.map((plate, index) => (
                   <Draggable
                     isDragDisabled={true}
-                    key={String(plate.appid)}
-                    draggableId={String(plate.appid)}
+                    key={plate.id}
+                    draggableId={plate.id}
                     index={index}
                   >
                     {(provided) => (
