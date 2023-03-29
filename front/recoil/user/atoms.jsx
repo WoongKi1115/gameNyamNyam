2574568041fca2788de44b66273d1c6780408207
nwk2222 <nwk2222@naver.com>
2023-03-29 09:49:57 +0900

    feat: changed data
    Former-commit-id: 31193ff5634fceed6ef4f850263a8811cd2a2714

--- front/recoil/user/atoms.jsx
+++ front/recoil/user/atoms.jsx
@@ -1,9 +1,20 @@
 import { atom } from 'recoil';
 
 export const DEFAULT_USERGAME = {
-  id: null,
-  name: null,
+  index: null,
+  appid: null,
   price: null,
+  about_the_game: null,
+  categories: null,
+  developers: null,
+  genres: null,
+  image: null,
+  metacritic: null,
+  name: null,
+  recommendations: null,
+  release_date: null,
+  screenshots: null,
+  short_description: null,
 };
 
 export const userGame = atom({
