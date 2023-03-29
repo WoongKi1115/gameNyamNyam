fe9628413ca6ae3b34fdcea0be894dd0ac398df8
kimchangyoung <kyle.kcy@gmail.com>
2023-03-29 15:49:26 +0900

    result
    Former-commit-id: 729f6d7009008af49f7f5f006e2aac7c7f22548f

--- front/src/components/AddGameModal.jsx
+++ front/src/components/AddGameModal.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ImgBox from './Imgbox';
 
-export default function gamemodal() {
+export default function gamemodal({setaddgame}) {
+  const closeaddgame = () => {
+    setaddgame(false);
+  }
   return (
-    <div className="absolute inset-x-0 bottom-0 flex justify-center space-x-3 bg-black bg-opacity-80 gap-4 py-4">
+    <div 
+    className="absolute inset-x-0 bottom-0 flex justify-center space-x-3 bg-black bg-opacity-80 gap-4 py-4"
+    onClick={closeaddgame}>
       <a href="https://www.naver.com" target="_blank" rel="noopener noreferrer" className='mx-4'>
         <ImgBox />
       </a>
