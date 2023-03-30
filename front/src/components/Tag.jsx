56acf2818f5886708980e96113a59bf911689806
nwk2222 <nwk2222@naver.com>
2023-03-30 12:00:31 +0900

    feat:game modal css
    Former-commit-id: c716c1800ad670d4d141aeeb4436d0a7398352d8

--- front/src/components/Tag.jsx
+++ front/src/components/Tag.jsx
@@ -4,7 +4,7 @@ export default function Tag(props) {
   console.log(props);
   return (
 
-      <kbd className="px-2 py-1.5 mb-2 font-semibold text-white bg-neutral-600 border rounded-full  text-sm">
+      <kbd className="px-2 py-1.5 mb-2 font-semibold text-white bg-neutral-600 border rounded-full text-sm">
         {props.props}
       </kbd>
 
