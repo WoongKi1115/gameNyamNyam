f4072dd961e7d54ee701da5483e402c2030bfe75
이수아 <huimhuimi@naver.com>
2023-03-30 14:14:12 +0900

    fix: import module
    Former-commit-id: af335da7668fbddce1990149c82d933ad5c69c6a

--- back/app/main.py
+++ back/app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from api.api import api_router
+# from api.api import api_router
 
 app = FastAPI(
-    title = "Game Nyamnyam API"
+    title="Game Nyamnyam API"
 )
 
 # CORS 설정
@@ -14,10 +14,10 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = origins,
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"],
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-app.include_router(api_router)
+# app.include_router(api_router)
