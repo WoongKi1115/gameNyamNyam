df1da589eba3d1b4b85fe8e9ea1644da42120612
nwk2222 <nwk2222@naver.com>
2023-03-30 11:31:43 +0900

    feat:modal css
    Former-commit-id: 6fc211a242ec432c98eb696673649cdd3493721c

--- front/src/components/Tag.jsx
+++ front/src/components/Tag.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 export default function Tag(props) {
+  console.log(props);
   return (
-    <kbd className="px-2 py-1.5 font-semibold text-white bg-neutral-600 border rounded-full">
-      # 임시 {props.type}
-    </kbd>
+
+      <kbd className="px-2 py-1.5 mb-2 font-semibold text-white bg-neutral-600 border rounded-full  text-sm">
+        {props.props}
+      </kbd>
+
   );
 }
