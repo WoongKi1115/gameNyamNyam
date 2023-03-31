9536c1eb17c74a611e1c4793d67b4c0a82a9a056
이수아 <huimhuimi@naver.com>
2023-03-31 17:03:24 +0900

    fix: back 수정
    Former-commit-id: 9d830ec3651613d24e0ca4c574872b2f84ff26f8

--- back/app/api/api.py
+++ back/app/api/api.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from api.endpoints import games, login
 
-api_router = APIRouter()
+api_router = APIRouter(prefix="/api")
 
 api_router.include_router(games.router, prefix="/games", tags=["games"])
 api_router.include_router(login.router, prefix="/login", tags=["login"])
