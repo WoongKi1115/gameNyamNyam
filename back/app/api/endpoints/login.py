a63b78a0556a08e1c6aaa0e268866e9043146a6c
이수아 <huimhuimi@naver.com>
2023-03-31 17:36:50 +0900

    fix: login api 수정
    Former-commit-id: 2277bfb420968aefb75d4a58da8d0523fb119e8a

--- back/app/api/endpoints/login.py
+++ back/app/api/endpoints/login.py
@@ -2,18 +2,19 @@ from starlette.requests import Request
 from steamsignin import SteamSignIn
 from fastapi import APIRouter, Depends
 
-router = APIRouter()
+router = APIRouter(prefix="/api")
 
-# api_url = "https://127.0.0.1:8000"
 api_url = "https://j8c204.p.ssafy.io"
 
 
 @router.get('/')
 async def main(steam_signin: SteamSignIn = Depends(SteamSignIn)):
     url = steam_signin.ConstructURL(api_url+'/processlogin')
+    print(url)
     return steam_signin.RedirectUser(url)
 
 
 @router.get('/processlogin')
 async def pr(request: Request, steam_signin: SteamSignIn = Depends(SteamSignIn)):
+    print("하이")
     return steam_signin.ValidateResults(request.query_params)
