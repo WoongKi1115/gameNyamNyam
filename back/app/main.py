4526f6a03afd5f33c8a3fe7c7a969fab16efb25b
이수아 <huimhuimi@naver.com>
2023-04-03 14:18:16 +0900

    fix: cors
    Former-commit-id: 722977e08e25523835b885aa9c60ec27c6e428b0

--- back/app/main.py
+++ back/app/main.py
@@ -12,7 +12,7 @@ origins = ["https://steamcommunity.com", "https://j8c204.p.ssafy.io"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
