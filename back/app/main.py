a24d100cc79aa103407749b7a632df03821249d7
이수아 <huimhuimi@naver.com>
2023-04-03 14:30:16 +0900

    fix: cors
    Former-commit-id: bbe13b85fd059ca03651c872bef9afee1114543c

--- back/app/main.py
+++ back/app/main.py
@@ -1,3 +1,6 @@
+from fastapi import APIRouter, Depends
+from steamsignin import SteamSignIn
+from starlette.requests import Request
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from api.api import api_router
@@ -12,9 +15,23 @@ origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(api_router)
+
+
+api_url = "https://j8c204.p.ssafy.io/api/login"
+
+
+@app.get('/api/login/')
+async def main(steam_signin: SteamSignIn = Depends(SteamSignIn)):
+    url = steam_signin.ConstructURL(api_url+'/processlogin')
+    return steam_signin.RedirectUser(url)
+
+
+@app.get('/api/login/processlogin')
+async def pr(request: Request, steam_signin: SteamSignIn = Depends(SteamSignIn)):
+    return steam_signin.ValidateResults(request.query_params)
