9697008cc58d7625963346facafc1bec59f5bd62
이수아 <huimhuimi@naver.com>
2023-04-03 17:42:34 +0900

    fix: redirect
    Former-commit-id: 4f4df60c61b11151ec1fff4cd7a26053906b0f82

--- back/app/api/endpoints/login.py
+++ back/app/api/endpoints/login.py
@@ -1,4 +1,5 @@
 from starlette.requests import Request
+from fastapi.responses import RedirectResponse
 from steamsignin import SteamSignIn
 from fastapi import APIRouter, Depends
 
@@ -7,14 +8,17 @@ router = APIRouter()
 api_url = "https://j8c204.p.ssafy.io/api/login"
 
 
-@router.get('/')
-async def main(steam_signin: SteamSignIn = Depends(SteamSignIn)):
-    url = steam_signin.ConstructURL(api_url+'/processlogin')
-    a = steam_signin.RedirectUser(url)
-    print("dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd", a)
-    return a
+# @router.get('/')
+# async def main(steam_signin: SteamSignIn = Depends(SteamSignIn)):
+#     url = steam_signin.ConstructURL(api_url+'/processlogin')
+#     a = steam_signin.RedirectUser(url)
+#     print("dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd", a)
+#     return a
 
 
 @router.get('/processlogin')
 async def pr(request: Request, steam_signin: SteamSignIn = Depends(SteamSignIn)):
-    return steam_signin.ValidateResults(request.query_params)
+    steam_id = steam_signin.ValidateResults(request.query_params)
+    redirect_url = "https://j8c204.p.ssafy.io?steam_id={}".format(steam_id)
+
+    return RedirectResponse(url=redirect_url)
