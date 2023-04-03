a2da495a8df48a6a8efe751ac68bc09eddbf7c55
leedoyeon <ldy__0202@naver.com>
2023-04-03 17:54:13 +0900

    fix: 경로 설정
    Former-commit-id: aa0654688cfec5ff610e3fa86b49df16e4d1b2e4 [formerly 7a5e9ad5960b081cb8d239aa9c9cbf0b24f29b14]
    Former-commit-id: ff725876a16a4e4bc81d81175b869fabb65aeb29

--- back/app/recommend/recommendation_game_final.py
+++ back/app/recommend/recommendation_game_final.py
@@ -2,23 +2,22 @@
 import urllib.request
 
 
-import matplotlib.pyplot as plt
 
 import time
 import io
 
-from PIL import Image
+
 from io import BytesIO
 
 from gensim.models import KeyedVectors
 
+import re
 
 '''
 #---------------------
 import pandas as pd
 import numpy as np
 
-import re
 
 import nltk
 from nltk.corpus import stopwords
@@ -45,7 +44,7 @@ steam_key = os.getenv('steam_key')
 nltk.download('stopwords')
 
 #-----------------------------------------------------------------------------------
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # (1) metacritc 있고 (2) 추천 수로 정렬된 데이터 (3) 200개 불러오기
 game_list = games.aggregate([
         { "$sort": { "recommendations": -1 } },
@@ -55,6 +54,8 @@ game_list = games.aggregate([
     ])
 game_list = pd.DataFrame(list(game_list))
 game_df = game_list.copy()
+
+
 
 # 데이터 정제
 
@@ -89,7 +90,6 @@ game_df['cleaned'] = game_df['cleaned'].apply(remove_punctuation)
 game_df['cleaned'].replace('', np.nan, inplace=True)
 game_df = game_df[game_df['cleaned'].notna()]
 print('Total number of documents after cleaning:', len(game_df))
-print(game_df)
 
 # # word2Vec 모델 만들기
 
@@ -101,7 +101,9 @@ print(game_df)
 corpus = [words.split() for words in game_df['cleaned']]
 word2vec_model = Word2Vec(vector_size=300, window=5, min_count=2, workers=-1) # size 300, window 5 설정
 word2vec_model.build_vocab(corpus)
-# word2vec_model.wv.intersect_word2vec_format('GoogleNews-vectors-negative300.bin', lockf=1.0, binary=True) # pre-trained data
+word2vec_model.wv.vectors_lockf = np.ones(len(word2vec_model.wv))
+# word2vec_model.wv.intersect_word2vec_format('GoogleNews-vectors-negative300.bin.gz', lockf=1.0, binary=True) # pre-trained data
+word2vec_model.wv.intersect_word2vec_format(BASE_DIR+'/recommend/GoogleNews-vectors-negative300.bin.gz', lockf=1.0, binary=True) # pre-trained data
 word2vec_model.train(corpus, total_examples=word2vec_model.corpus_count, epochs=15)
 
 
@@ -114,14 +116,10 @@ word2vec_model.train(corpus, total_examples=word2vec_model.corpus_count, epochs=
 # append를 다 안 한다?
 # 각 문서의 벡터를 추출, 이를 이용하여 유사도 행렬 계산
 # Get document vectors
-def get_document_vectors(document_list, model):
+index_list_result = []
+def get_document_vectors(index_list, document_list, model):
     document_embedding_list = []
-    mytest_index = []
-    
-    for row in document_list:
-        print("row")
-        print(row)
-        line = row['cleaned']
+    for index, line in document_list.iteritems():
         doc2vec = None
         count = 0
         for word in line.split():
@@ -134,17 +132,14 @@ def get_document_vectors(document_list, model):
         if doc2vec is not None:
             doc2vec = doc2vec / count
             document_embedding_list.append(doc2vec)
-            mytest_index.append(line.index)
-        else:
-            print("있어")
-            print(line)
+            index_list_result.append(index)
+
     
     return document_embedding_list
 
-document_embedding_list = get_document_vectors(game_df, word2vec_model)
+document_embedding_list = get_document_vectors(game_df.index.to_list(), game_df['cleaned'], word2vec_model)
 print('Number of document vectors:', len(document_embedding_list))
 #print(document_embedding_list)
-
 
 # Calculate cosine similarity matrix
 cosine_similarities = cosine_similarity(document_embedding_list, document_embedding_list)
@@ -156,15 +151,15 @@ print(pd.DataFrame(cosine_similarities))
 
 # 게임에 대한 appid를 추출하는 함수
 
-def get_game_indices(name, df, cosine_similarities):
+def get_game_indices(appid, df, cosine_similarities):
     # Get the index of the game based on its name
-    indices = pd.Series(df.index, index=df['name']).drop_duplicates()
-    idx = indices[name]
+    indices = pd.Series(df.index, index=df['appid']).drop_duplicates()
+    idx = indices[appid]
     print(idx)
-    
+    sim_index = index_list_result[idx]
     # Get the indices of the most similar games based on the cosine similarities
     # 하나의 게임에 대한 모든 코사인 유사도 
-    sim_scores = list(enumerate(cosine_similarities[idx]))
+    sim_scores = list(enumerate(cosine_similarities[sim_index]))
     sim_scores = sorted(sim_scores,reverse=True)
     sim_scores = sim_scores[1:21]
     print("sim_scores")
@@ -174,10 +169,10 @@ def get_game_indices(name, df, cosine_similarities):
     return game_indices
 
 # 선택한 게임에 대한 정보를 추출하는 함수
-def info_recommendations(name, df, cosine_similarities):
-    games = df[['name']]
+def info_recommendations(appid, df, cosine_similarities):
+    games = df[['appid']]
     # Get the indices of the recommended games
-    game_indices = get_game_indices(name, df, cosine_similarities)
+    game_indices = get_game_indices(appid, df, cosine_similarities)
     print("game_indices!!!!!!!!!")
     print(game_indices)
     
@@ -190,7 +185,7 @@ def info_recommendations(name, df, cosine_similarities):
 # -------------------------------------input으로 줘야지. --------------------------------------------------
 
 users = {"sua":"76561198797386305","ong":"76561198099903362","yeon":"76561198135409603"}
-appid=users["sua"]
+appid=users["yeon"]
 
 res = requests.get(f'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={steam_key}&steamid={appid}').json()
 
@@ -215,7 +210,7 @@ for appid in owned_games:
     if(game_df[game_df['appid']==appid].empty):
         continue
     else:
-        appid_list.append(temp['name'].values[0])
+        appid_list.append(temp['appid'].values[0])
 
 print('DB와 일치하는 유저의 게임 개수 : ',len(appid_list))
 print('DB와 일치하는 유저의 게임 리스트 : ',appid_list)
@@ -229,10 +224,10 @@ print('DB와 일치하는 유저의 게임 리스트 : ',appid_list)
 # 추천 결과를 저장할 데이터프레임을 생성합니다.
 recommend_df = pd.DataFrame()
 
-for game_name in appid_list:
-    print(game_name)
+for game_id in appid_list:
+    print(game_id)
     try:
-        recommend_info = info_recommendations(game_name, game_df, cosine_similarities)
+        recommend_info = info_recommendations(game_id, game_df, cosine_similarities)
         game_list = pd.concat(([game_list,recommend_info]))
         
     except Exception as e:
@@ -249,5 +244,7 @@ print('추천 게임 후보 개수(중복제외) : ', len(random_games))
 # # 후보 군에서 랜덤으로 60개를 비복원 추출한다.
 
 # 복원추출을 하려면 replace=True
-random_games = random_games.sample(n=60)
+random_games = random_games.sample(n=30)
 print(random_games['appid'].tolist())
+'''
+'''
