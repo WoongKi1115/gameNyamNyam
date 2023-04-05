c63e4a0cac296661152b31080ce639cec81c9e53
nwk2222 <nwk2222@naver.com>
2023-04-05 17:58:56 +0900

    feat:tutor
    Former-commit-id: b4782afbcfcc2b8a8fd1ab066d20c6f17fff3feb [formerly 331301c5f1f0bc54b762925f0edb49766fd63b9d]
    Former-commit-id: 0680f2c94d7a09bf830129028b60da8ff6ec2955

--- front/src/components/TutorialCount.jsx
+++ front/src/components/TutorialCount.jsx
@@ -36,6 +36,29 @@ export default function TutorialCount({ count, setCount }) {
         <div className="ml-[13vw]">
           <div className="pointingCircle border-white border-2 border-dashed"></div>
           <div className="ml-[140px]">
+            <div className="bg-white w-[400px] h-[80px]  mt-3 pt-1 px-4 rounded-md">
+              <p className="mt-2">
+                2. 하고 싶은 게임이 보이면 아래 테이블로 드래그 해주세요!
+              </p>
+            </div>
+            <button
+              className="mt-2 ml-[200px] bg-yellow-300 hover:bg-yellow-500 font-bold rounded-lg px-2 py-1"
+              onClick={plusCount}
+            >
+              다음
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  } else if (count === 3) {
+    return (
+      <div>
+        <div className="h-[36vh]"></div>
+        <div className="h-[15vh]"></div>
+        <div className="ml-[13vw]">
+          <div className="pointingCircle border-white border-2 border-dashed"></div>
+          <div className="ml-[140px]">
             <div className="bg-white w-[250px] h-[80px]  mt-3 pt-1 px-2 rounded-md">
               <p className="mt-2">
                 2. 하고 싶은 게임이 보이면 아래 테이블로 드래그 해주세요!
